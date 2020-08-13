0e89cceb251da84f6ff8d3e0bfe5cc16f43eeda9
RanganathanKP <ranganathankp@gmail.com>
2020-08-13 17:18:17 +0530

    added entity

--- src/main/java/com/spares/customer/CustomerApplication.java
+++ src/main/java/com/spares/customer/CustomerApplication.java
@@ -4,11 +4,13 @@ import com.spares.customer.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
+@EnableFeignClients("com.spares.customer")
 public class CustomerApplication {
 
 	public static void main(String[] args) {
